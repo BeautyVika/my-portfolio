fa772def8e09277ac2fbf46839fe3bfbf56de396
BeautyVika <vikachk54@gmail.com>
2023-02-10 17:26:13 +0300

    refactor Title

--- src/components/Common/components/Title/Title.tsx
+++ src/components/Common/components/Title/Title.tsx
@@ -5,7 +5,11 @@ function Title(props: {mainTitle: string, title: string}) {
     return (
         <div className={style.containerTitle}>
             <h2>{props.mainTitle}</h2>
-            <p className={style.titleProjects}>{props.title}</p>
+            <p className={style.titleProjects}>
+                {props.title}
+                <span className={style.line}></span>
+            </p>
+            {/*<span className={style.line}></span>*/}
         </div>
 
     )
