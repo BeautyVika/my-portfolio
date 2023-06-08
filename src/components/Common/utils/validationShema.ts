f248c0b01d543b0d75daaa12d0a0dccf2ddc2f7c
BeautyVika <vikachk54@gmail.com>
2023-06-08 12:18:11 +0300

    added validation errors

--- src/components/Common/utils/validationShema.ts
+++ src/components/Common/utils/validationShema.ts
@@ -5,13 +5,13 @@ const MIN_MESSAGE_LENGTH = 12
 export const validationSchema = yup.object({
     name: yup
         .string()
-        .required('Name is required'),
+        .required('Name is required ❗'),
     email: yup
         .string()
         .email('Enter a valid email')
-        .required('Email is required'),
+        .required('Email is required❗'),
     message: yup
         .string()
         .min(MIN_MESSAGE_LENGTH, `Message should be of minimum ${MIN_MESSAGE_LENGTH} characters length`)
-        .required('Message is required'),
+        .required('Message is required ❗'),
 });
--- src/components/Contacts/ContactForm/ContactForm.tsx
+++ src/components/Contacts/ContactForm/ContactForm.tsx
@@ -25,16 +25,30 @@ export const ContactForm: FC = () => {
         },
         validationSchema: validationSchema,
         onSubmit: (values) => {
+            setOnDisabled(true)
             axios.post('https://node-api-vercel-contact.vercel.app/sendMessage',{
                 email: values.email,
                 name: values.name,
                 message: values.message,
                 }
             )
-                .then((res)=> alert('Your message has been sent'))
-                .catch((err)=> alert('Your message has not been sent'))
+                .then((res) => {
+                    setSnackbarMessage("Your message has been sent successfully")
+                    setSnackbarType('success')
+                    formik.resetForm()
+                })
+                .catch((err)=> {
+                    setSnackbarMessage("Something went wrong...Your message was not sent")
+                    setSnackbarType('error')
+                })
+                .finally(() => {
+                    setSnackbarShow(true)
+                    setOnDisabled(false)
+                })
         }
     })
+
+    const finalClassNames = onDisabled ? `${style.disable} ${style.btn}` : style.btn
     return (
         <>
             <form className={style.contactsForm} onSubmit={formik.handleSubmit}>
@@ -46,6 +60,11 @@ export const ContactForm: FC = () => {
                     placeholder='Name'
                     onChange={formik.handleChange}
                 />
+                {formik.touched.email && formik.errors.email && (
+                    <div className={style.error}>
+                        {formik.errors.name}
+                    </div>
+                )}
                 <input
                     type="text"
                     name='email'
@@ -54,6 +73,11 @@ export const ContactForm: FC = () => {
                     value={formik.values.email}
                     onChange={formik.handleChange}
                 />
+                {formik.touched.email && formik.errors.email && (
+                    <div className={style.error}>
+                        {formik.errors.email}
+                    </div>
+                )}
                 <textarea
                     name='message'
                     className={`${style.text} ${style.input}`}
@@ -61,10 +85,16 @@ export const ContactForm: FC = () => {
                     value={formik.values.message}
                     onChange={formik.handleChange}
                 />
+                {formik.touched.email && formik.errors.email && (
+                    <div className={style.error}>
+                        {formik.errors.message}
+                    </div>
+                )}
 
                 <button
                     type="submit"
-                    className={style.btn}
+                    className={finalClassNames}
+                    disabled={onDisabled}
                 >
                     Send message
                 </button>
