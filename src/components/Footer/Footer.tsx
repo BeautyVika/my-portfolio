2a69b5244f79b66ab9f5f42cdffb8f48e46bd170
BeautyVika <vikachk54@gmail.com>
2023-06-08 12:42:25 +0300

    refactoring styles

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,7 +2,6 @@ import React, {FC} from "react"
 import style from './Footer.module.scss'
 import styleContainer from "../Common/Styles/Container.module.scss"
 import {ContactButtonGroup} from "../ContactButtonGroup/ContactButtonGroup"
-import {BsArrowRight} from "@react-icons/all-files/bs/BsArrowRight"
 
 export const Footer: FC = () => {
     return (
@@ -13,11 +12,7 @@ export const Footer: FC = () => {
                     <p className={style.title}>Belarus, Minsk</p>
                 </div>
 
-                <div className={style.find}>
-                    <p className={style.title}>Find me</p>
-                    <BsArrowRight className={style.row}/>
-                    <ContactButtonGroup/>
-                </div>
+                <ContactButtonGroup/>
 
             </div>
         </footer>
